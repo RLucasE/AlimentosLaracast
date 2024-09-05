4a03882d4610a0d0be0b1849192413e8a1048d66
RLucasE <lucascabjnmro2@gmail.com>
2024-09-05 02:10:52 -0300

    form terminado

--- segunda-aplicacion/src/App.js
+++ segunda-aplicacion/src/App.js
@@ -1,26 +1,32 @@
 import './App.css';
-import React, {useState} from "react"
+import React, { useRef } from "react"
+import video from "./assets/video.mp4"
 
 function App() {
-  const [resultado,setResultado] = useState(null);
-  const titulo = <h1 className="centrar-texto">Titulo</h1>;
- /*const suma = <h1>{sumar(2, 3)} Comentario en js</h1>*/
-  const botonPulsado = ()=>{
-    const result = sumar(2,2)
-    setResultado(result);
-  }
+  const videoRef = useRef(null);
+
+  const videoPlay = () => {  
+    videoRef.current.play()
+  };
+
+  const videoPause = () => {  
+    videoRef.current.pause()
+  };
 
   return (
     <div>
-      <button onClick={botonPulsado} style={{margin: "1em"}}>Pulsame</button>
-      <div>{titulo}</div>
-      <div>{resultado!=null && <div>El resultado es: {resultado}</div>}</div>
-    </div>)
+
+      <video ref={videoRef} width="" height="" controls>
+        <source src={video} type="video/mp4" />
+      </video>
+      <div>
+        <button onClick={videoPlay}>Play</button>
+        <button onClick={videoPause}>Pause</button>
+      </div>
+    </div>
+  );
 }
 
-function sumar(a, b) {
-  return a + b;
-}
 
 export default App;
 
