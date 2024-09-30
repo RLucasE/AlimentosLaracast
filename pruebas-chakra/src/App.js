6b8ee05f7264a17893316fe0e113fe318dd95b2e
RLucasE <lucascabjnmro2@gmail.com>
2024-09-30 12:17:09 -0300

    Hola mundo del AdminLTE creadao

--- pruebas-chakra/src/App.js
+++ pruebas-chakra/src/App.js
@@ -1,116 +1,97 @@
-import './App.css';
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import { Input } from '@chakra-ui/react'
-import { useState } from 'react'
-import axios from 'axios';
+import "./App.css";
+import * as React from "react";
+//import { ChakraProvider } from "@chakra-ui/react";
+//import { Input } from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+// import {
+//   FormControl,
+//   FormLabel,
+//   FormErrorMessage,
+//   FormHelperText,
+//   HStack,
+//   Radio,
+//   RadioGroup,
+//   InputGroup,
+//   InputLeftElement,
+//   Button,
+// } from "@chakra-ui/react";
 import {
+  ChakraProvider,
+  Box,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-  HStack,
-  Radio,
-  RadioGroup,
-  InputGroup,
-  InputLeftElement,
-  Button
-} from '@chakra-ui/react'
-
-import { Icon } from '@chakra-ui/icons'
-import { MdPhone } from 'react-icons/md'
+  Input,
+  Textarea,
+  Button,
+  useToast,
+} from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/icons";
+import { MdPhone } from "react-icons/md";
 
 function App() {
-  const [input, setInput] = useState('')
-  const [isTouched, setIsTouched] = useState(false);
-  const handleInputChange = (e) => {
-    setInput(e.target.value)
+  const [formData, setFormData] = React.useState({
+    email: "",
+    subject: "",
+    message: "",
+  });
 
-  }
-  const isError = (input === '' && isTouched)
+  const toast = useToast();
 
-  const [data, setData] = useState(null);
-
-  const sendData = async () => {
-    try {
-      const response = await axios.post('http://localhost/api.php', {
-        key: 'value'
-      });
-      setData(response.data);
-    } catch (error) {
-      console.error('Error al enviar los datos', error);
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
-  const handleSubbmit = () => {
-    
-  }
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <ChakraProvider>
-
-      <div className='main-top-container'>
-
-        <FormControl className='container margin-block' >
-        
-          <div className='item'>
-            <Input placeholder='Nombre' className='space' />
-            <Input placeholder='Apellido' className='space' />
-            <FormControl isInvalid={isError}>
-              <FormLabel className='space' isRequired>Email</FormLabel>
-              <Input type='email' value={input} onChange={handleInputChange} />
-              {!isError ? (
-                <FormHelperText>
-                  Nunca compartiremos tu email
-                </FormHelperText>
-              ) : (
-                <FormErrorMessage>Ingrese un Email</FormErrorMessage>
-              )}
+      <div className="main-top-container">
+        <Box p={8} maxWidth="600px" mx="auto">
+          <form onSubmit={handleSubmit}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Correo</FormLabel>
+              <Input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
             </FormControl>
 
+            <FormControl id="subject" mt={4}>
+              <FormLabel>Asunto</FormLabel>
+              <Input
+                type="text"
+                name="subject"
+                value={formData.subject}
+                onChange={handleChange}
+              />
+            </FormControl>
 
-          </div>
+            <FormControl id="message" mt={4}>
+              <FormLabel>Mensaje</FormLabel>
+              <Textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+              />
+            </FormControl>
 
-          <div className='item'>
-            <FormLabel as='legend'>Idioma secundario</FormLabel>
-            <RadioGroup defaultValue='Itachi'>
-              <HStack spacing='24px'>
-                <Radio value='Ingles'>Ingles</Radio>
-                <Radio value='Portugues'>Portugues</Radio>
-                <Radio value='Frances'>Frances</Radio>
-              </HStack>
-            </RadioGroup>
-          </div>
-
-          <div className='item'>
-            <InputGroup>
-              <InputLeftElement pointerEvents='none'>
-                <Icon as={MdPhone} color='gray.300' />
-              </InputLeftElement>
-              <Input type='tel' placeholder='Phone number' />
-            </InputGroup>
-          </div>
-
-          <div className='item'>
-            <FormLabel>Fecha de nacimiento</FormLabel>
-            <Input placeholder='Select Date and Time' size='md' type='date' />
-          </div>
-          <Button mt={4} onClick={() => setIsTouched(true)}>
-            Submit
-          </Button> 
-
-
-
-        </FormControl>
-
-        <button onClick={sendData}>Enviar Datos</button>
-        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-
+            <Button type="submit" colorScheme="teal" mt={4}>
+              Enviar
+            </Button>
+          </form>
+        </Box>
       </div>
-
     </ChakraProvider>
-  )
+  );
 }
 
 export default App;
