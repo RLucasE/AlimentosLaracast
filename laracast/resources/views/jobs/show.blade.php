2f7e22e518311af842ebfd712080d8aa3839a1c8
RLucasE <lucascabjnmro2@gmail.com>
2024-10-30 13:12:07 -0300

    Commit antes de presentar el avance

--- laracast/resources/views/jobs/show.blade.php
+++ laracast/resources/views/jobs/show.blade.php
@@ -4,4 +4,7 @@
     </x-slot:heading>
     <h2>{{ $job['title'] }}</h2>
     <h3>This job pays {{ $job['salary'] }}</h3>
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Editar</x-button>
+    </p>
 </x-layout>
--- laracast/routes/web.php
+++ laracast/routes/web.php
@@ -9,15 +9,56 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(8);
-    return view('jobs', [
+    $jobs = Job::with('employer')->latest()->simplePaginate(8);
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+Route::post('/jobs', function () {
+
+    request()->validate([
+        'title' => 'required|min:5',
+        'salary' => 'required|numeric'
+    ]);
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
+});
+
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => 'required|min:5',
+        'salary' => 'required|numeric'
+    ]);
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
